6e860986503713bca80a8f72f0331ed05fff3431
Kyuzo-sama <56452203+Kyuzo-sama@users.noreply.github.com>
2020-05-01 17:45:20 +0100

    Create Player.cpp

---
+++ Classes/Player.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Administrator on 29/04/2020.
+//
+
+#include "Player.h"
+
+
+Player::Player(unsigned int id, std::vector<char> hand,  unsigned int score = 0) {
+    this->id = id;
+    this->score = score;
+    this->hand = hand;
+}
+
+unsigned int Player::getID() const {
+    return id;
+}
+
+std::vector<char> Player::getHand() const {
+    return hand;
+}
+
+unsigned int Player::getScore() const {
+    return score;
+}
+
+void Player::setHand(std::vector<char> hand) {
+    //to be filled
+}
+
+void Player::setID(unsigned int id) {
+    this->id = id;
+}
+
+void Player::setScore(unsigned int score) {
+    this->score = score;
+}
