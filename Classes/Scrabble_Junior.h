99a96dcd272bcbba913a45c7dc8fd2ca0a93b43f
Kyuzo-sama <56452203+Kyuzo-sama@users.noreply.github.com>
2020-05-01 17:45:28 +0100

    Create Scrabble_Junior.h

---
+++ Classes/Scrabble_Junior.h
@@ -0,0 +1,24 @@
+//
+// Created by Administrator on 29/04/2020.
+//
+
+#ifndef SCRABBLEJUNIOR_SCRABBLE_JUNIOR_H
+#define SCRABBLEJUNIOR_SCRABBLE_JUNIOR_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Player.h"
+
+
+class Game{
+
+public:
+
+private:
+    //std::vector<Board> boards;
+    std::vector<Player> players;
+
+};
+
+#endif //SCRABBLEJUNIOR_SCRABBLE_JUNIOR_H
