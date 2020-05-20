6a53b909350de16dbbbc6d293c6698a56ae2b763
Kyuzo-sama <56452203+Kyuzo-sama@users.noreply.github.com>
2020-05-20 22:30:38 +0100

    Update PlayerScoreService.cpp

--- ScrabbleJunior/Services/PlayerScoreService.cpp
+++ ScrabbleJunior/Services/PlayerScoreService.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "PlayerScoreService.h"
+
+
+void PlayerScoreService::call(Player &player, Tile &tile, Board &board){
+
+    unsigned short score = player.getScore();
+
+    std::vector<unsigned short> wordIds = tile.getWordIds();
+
+
+    for(auto x: wordIds){
+        
+        Word w = board.getWord(x);
+
+        if(w.getAvailableLetters().size() == 0)++score;
+
+    }
+    player.setScore(score);
+
+}
