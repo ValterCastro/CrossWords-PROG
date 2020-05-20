14cd03c1cf57ececcf5ea738131c26f16b36e426
Kyuzo-sama <56452203+Kyuzo-sama@users.noreply.github.com>
2020-05-20 22:30:43 +0100

    Update PlayerScoreService.h

--- ScrabbleJunior/Services/PlayerScoreService.h
+++ ScrabbleJunior/Services/PlayerScoreService.h
@@ -5,4 +5,17 @@
 #ifndef SCRABBLEJUNIOR_PLAYERSCORESERVICE_H
 #define SCRABBLEJUNIOR_PLAYERSCORESERVICE_H
 
+#include <string>
+#include <vector>
+
+#include "../Classes/Player.h"
+#include "../Classes/Tile.h"
+#include "../Classes/Word.h"
+#include "../Classes/Board.h"
+
+class PlayerScoreService{
+public:
+
+    void static call(Player &player, Tile &tile, Board &board);
+
 #endif //SCRABBLEJUNIOR_PLAYERSCORESERVICE_H
